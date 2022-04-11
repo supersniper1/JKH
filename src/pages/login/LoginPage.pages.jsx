59805f82a9c663ca6028d2e773353d21f6c02df0
Егор <egornesvickij@gmail.com>
2022-04-11 13:53:38 +0500

    added routes

--- src/pages/login/LoginPage.pages.jsx
+++ src/pages/login/LoginPage.pages.jsx
@@ -1,29 +1,32 @@
 import React from "react";
-import styles from './loginPage.module.sass';
+import { useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../utils/auth/useAuth";
 
 export const LoginPage = () => {
-  // let history = useHistory();
-  // let location = useLocation();
-  // let auth = useAuth();
 
-  // let { from } = location.state || { from: { pathname: "/" } };
-  // let login = () => {
-  //   auth.signin(() => {
-  //     history.replace(from);
-  //   });
-  // };
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { signIn } = useAuth();
+
+  const fromPage = location.state?.from?.pathname || '/';
+
+  const user = 'den';
+
+  const handleLogin = () => {
+    signIn(user, () => navigate(fromPage, { replace: true }))
+  }
 
   return (
     <div className="loginContainer">
       <div className="loginBlock">
-        <h5 className="joinText">Вход</h5>
+        <h5 className="joinText">{fromPage}</h5>
         <div className="loginContent">
           <h5 className="loginText">Имя:</h5>
           <input type="name" className="loginInput" />
           <h5 className="loginText">Пароль:</h5>
           <input type="password" className="loginInput" />
           <div>
-            <a type="button" className="loginButton">
+            <a type="button" className="loginButton" onClick={handleLogin}>
               Продолжить
             </a>
           </div>
