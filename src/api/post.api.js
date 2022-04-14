6e81c3b8a851aac9c28242dd6b5b41b91b3c3216
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-04-14 23:33:16 +0500

    login post requests to server was done

--- src/api/post.api.js
+++ src/api/post.api.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-axios(config)
 
 // http://10.3.21.206:8080/api/v1/auth/login
 
 const apiURL = 'http://10.3.21.206:8080'
-const postApi = () => {
+const postApi = (dataValue) => {
   axios({
     method: 'post',
-    url: apiURL,
-    data: {
-      firstName: 'Fred',
-      lastName: 'Flintstone'
-    }
+    url: apiURL + '/api/v1/auth/login',
+    data: dataValue
+    // !data example!
+    // data: {
+    //   id: 0,
+    //   firstName: 'Freddy',
+    //   lastName: 'Gayazov',
+    //   age: 45
+    // }
   });
 }
+
+export default postApi
--- src/pages/login/LoginPage.pages.jsx
+++ src/pages/login/LoginPage.pages.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./loginPage.sass";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../utils/auth/useAuth";
+import postApi from "../../api/post.api";
 
 export const LoginPage = () => {
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+
   const navigate = useNavigate();
   const location = useLocation();
   const { signIn } = useAuth();
@@ -20,17 +24,24 @@ export const LoginPage = () => {
     <div className="loginContainer">
       <div className="loginBlock">
         <h5 className="joinText">Вход</h5>
-        <div className="loginContent">
+        <form className="loginContent" onSubmit={(e) => {
+          e.preventDefault()
+          postApi({
+            login: login,
+            password: password
+          })
+          setPassword('')
+          setLogin('')
+        }}>
           <h5 className="loginText">Имя:</h5>
-          <input type="name" className="loginInput" />
+          <input type="name" className="loginInput" value={login} onChange={event => setLogin(event.target.value)}/>
           <h5 className="loginText">Пароль:</h5>
-          <input type="password" className="loginInput" />
-          <div>
-            <a type="button" className="loginButton" onClick={handleLogin}>
-              Продолжить
-            </a>
-          </div>
-        </div>
+          <input type="password" className="loginInput" value={password} onChange={event => setPassword(event.target.value)} />
+          <button className="loginButton" >Продолжить</button>
+          {/*<a type="button" className="loginButton" onClick={handleLogin}>*/}
+            {/*  Продолжить*/}
+            {/*</a>*/}
+        </form>
       </div>
     </div>
   );
