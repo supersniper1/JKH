a64199f835569d9d6a4927a0180c9a4029a4f94c
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-04-20 16:53:19 +0500

    comments was deleted

--- src/api/get.api.js
+++ src/api/get.api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-
-// http://10.3.21.206:8080/api/v1/requests
-
 const apiURL = 'http://10.3.21.206:8080'
 export const getRequests = (action, secondAction) => {
   axios.get(`${apiURL}/api/v1/requests`, {
