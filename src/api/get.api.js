3af99aadbd7973af8e7c5ded2d6624dbc6ee182f
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-04-20 12:54:23 +0500

    export was done not default

--- src/api/get.api.js
+++ src/api/get.api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // http://10.3.21.206:8080/api/v1/requests
 
 const apiURL = 'http://10.3.21.206:8080'
-const getRequests = (action, secondAction) => {
+export const getRequests = (action, secondAction) => {
   axios.get(`${apiURL}/api/v1/requests`, {
     headers: {
       "Authorization": localStorage.getItem("token")
@@ -13,5 +13,3 @@ const getRequests = (action, secondAction) => {
     .then(action)
     .then(secondAction)
 }
-
-export default getRequests
