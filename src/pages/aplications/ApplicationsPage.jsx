0cc7c34b3e5c5e6bfb4bedfb288cdc7cad6bfe2e
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-04-20 12:53:01 +0500

    fixed map names

--- src/pages/aplications/ApplicationsPage.jsx
+++ src/pages/aplications/ApplicationsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './applicationPage.sass';
 import { Application, Header, ModalApplication, ModalComponents } from "../../components/export.components";
-import getRequests from "../../api/get.api";
+import {getRequests} from "../../api/export.api";
 
 export const ApplicationsPage = () => {
   const [tickets, setTickets] = useState(null)
@@ -19,9 +19,9 @@ export const ApplicationsPage = () => {
       <div className="applications">
         {
           tickets ? (
-            tickets.map((ticket, index) => (
+            tickets.map((ticket) => (
               <Application
-                key={index}
+                key={ticket.id}
                 content={ticket}
               />
             ))
@@ -34,46 +34,3 @@ export const ApplicationsPage = () => {
     </div>
   );
 };
-
-
-// тестовые данные
-
-// const data = [{
-//   "id": 1,
-//   "status": "В РАБОТЕ",
-//   "street": "ул. Пушкина д.10",
-//   "area": "р-он Ленинский",
-//   "problem": "Система водоснобжения",
-//   "priority": 0,
-//   "date": "20.07.2022",
-//   "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquid asperiores consectetur cupiditate deleniti doloremque et exercitationem expedita facere facilis hic magnam nisi perferendis, perspiciatis qui quia quibusdam sunt tenetur voluptas. Adipisci assumenda blanditiis cum expedita necessitatibus sit, suscipit."
-// },
-// {
-//   "id": 1,
-//   "status": "В РАБОТЕ",
-//   "street": "ул. Пушкина д.10",
-//   "area": "р-он Ленинский",
-//   "problem": "Система водоснобжения",
-//   "priority": 1,
-//   "date": "20.07.2022",
-//   "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquid asperiores consectetur cupiditate deleniti doloremque et exercitationem expedita facere facilis hic magnam nisi perferendis, perspiciatis qui quia quibusdam sunt tenetur voluptas. Adipisci assumenda blanditiis cum expedita necessitatibus sit, suscipit."
-// }, {
-//   "id": 1,
-//   "status": "В РАБОТЕ",
-//   "street": "ул. Пушкина д.10",
-//   "area": "р-он Ленинский",
-//   "problem": "Система водоснобжения",
-//   "priority": 2,
-//   "date": "20.07.2022",
-//   "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquid asperiores consectetur cupiditate deleniti doloremque et exercitationem expedita facere facilis hic magnam nisi perferendis, perspiciatis qui quia quibusdam sunt tenetur voluptas. Adipisci assumenda blanditiis cum expedita necessitatibus sit, suscipit."
-// }, {
-//   "id": 1,
-//   "status": "В РАБОТЕ",
-//   "street": "ул. Пушкина д.10",
-//   "area": "р-он Ленинский",
-//   "problem": "Система водоснобжения",
-//   "priority": 3,
-//   "date": "20.07.2022",
-//   "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquid asperiores consectetur cupiditate deleniti doloremque et exercitationem expedita facere facilis hic magnam nisi perferendis, perspiciatis qui quia quibusdam sunt tenetur voluptas. Adipisci assumenda blanditiis cum expedita necessitatibus sit, suscipit."
-// }]
-
