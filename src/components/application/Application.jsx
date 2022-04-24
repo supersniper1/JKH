6af4364e7b31a386828e642a1424f51c56f03cf1
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-04-24 11:30:44 +0500

    Application.jsx properties names was changed with a new Api documentation

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import './application.sass';
 
-export const Application = ({ content }) => {
+export const Application = ({content}) => {
   let text = content.description
 
-  let priority = content.priority
+  let priority = content.userRequestPriority.id
   const priorityText = () => {
     switch (priority) {
-      case 1: return "экстренно";
-      case 2: return "важно";
-      case 3: return "нормально";
-      case 4: return "нейтрально";
+      case 1:
+        return "экстренно";
+      case 2:
+        return "важно";
+      case 3:
+        return "нормально";
+      case 4:
+        return "нейтрально";
     }
   }
 
   const priorityClass = () => {
     switch (priority) {
-      case 1: return "urgentlyPriority"
-      case 2: return "importantPriority"
-      case 3: return "normalPriority"
-      case 4: return "neutralPriority"
+      case 1:
+        return "urgentlyPriority"
+      case 2:
+        return "importantPriority"
+      case 3:
+        return "normalPriority"
+      case 4:
+        return "neutralPriority"
     }
   }
   return (
-    <div className="application">
+    <div className="application" onClick={() => console.log('sad')}>
       <div className="applicationColumns">
         <p className={priorityClass() + " applicationPriority"}>{priorityText()}</p>
         <h3>Заявка №{content.id}</h3>
         <p className="applicationDate">{content.dateToCreate === null ? "have not date" : content.dateToCreate}</p>
       </div>
       <div className="applicationColumns applicationColumnSecond">
-        <h3>{content.type}</h3>
+        <h3>{content.userRequestType.typeName}</h3>
         <p className="applicationText">{text}</p>
       </div>
       <div className="applicationColumns">
