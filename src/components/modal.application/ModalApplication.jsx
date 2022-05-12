376086cf1e437d8df902cdc33586b023436f4094
BogdanDjun <i21s045@students.it-college.ru>
2022-05-12 19:56:57 +0500

    little changes

--- src/components/modal.application/ModalApplication.jsx
+++ src/components/modal.application/ModalApplication.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './ModalApplication.sass';
 import {useSelector} from "react-redux";
+// import Slider from '../modal/slider';
 
 export const ModalApplication = () => {
   const [inputValue, setInputValue] = useState('')
@@ -24,6 +25,7 @@ export const ModalApplication = () => {
           <div className="attachments">
             <h2 className="titleH2">Вложения</h2>
             <div className="attachmentsSlider">
+              {/* <Slider /> */}
               <img src="http://via.placeholder.com/200x200"/>
               <img src="http://via.placeholder.com/200x200"/>
               <img src="http://via.placeholder.com/200x200"/>
@@ -68,7 +70,7 @@ export const ModalApplication = () => {
             </div>
           </div>
           <div className="rightBlockButtons">
-            <button>Сохранить</button>
+            <button className='modalButton'>Сохранить</button>
             <button>Закрыть</button>
           </div>
         </div>
@@ -76,51 +78,3 @@ export const ModalApplication = () => {
     </div>
   );
 };
-
-
-// import React, {useState} from 'react';
-// import './ModalApplication.sass';
-//
-// export const ModalApplication = ({setActive}) => {
-//   const [inputValue, setInputValue] = useState('')
-//   return (
-//     <div className="modalApplication">
-//       <h3>Ул. Ленина д. 38 стр. 1</h3>
-//       <div className='modalContentText'>
-//         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequuntur eligendi excepturi facilis inventore nihil quam saepe sapiente vel veniam. Dolor eos eum ex fugit impedit maiores, pariatur? Asperiores, dolores dolorum ea harum laboriosam nam nihil nisi odit sapiente ut.</p>
-//       </div>
-//       <div className='rows'>
-//         <div className="images">
-//           <div className="applicationImages">
-//             <div className='applicationImagesRow'>
-//               <img className='modalImage' src={require("../../images/img.png")} alt="Фото с места"/>
-//               <img className='modalImage' src={require("../../images/img.png")} alt="Фото с места"/>
-//             </div>
-//             <div className='applicationImagesRow'>
-//               <img className='modalImage' src={require("../../images/img.png")} alt="Фото с места"/>
-//               <img className='modalImage' src={require("../../images/img.png")} alt="Фото с места"/>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="commentAndList">
-//           <select className='modalSelect'>
-//             <option>Бригада 1</option>
-//             <option>Бригада 2</option>
-//             <option>Бригада 3</option>
-//             <option>Бригада 4</option>
-//             <option>Бригада 5</option>
-//           </select>
-//           <textarea className='modalInput' placeholder='Напишите комментарий...' value={inputValue}
-//                  onChange={event => setInputValue(event.target.value)} />
-//         </div>
-//       </div>
-//       <div className='rows rowLast'>
-//         <h3>Дворник Иван Иванович</h3>
-//         <button className="modalButton" onClick={() => {
-//           setActive(false)
-//           setInputValue('')
-//         }}>Отправить</button>
-//       </div>
-//     </div>
-//   );
-// };
---
+++ src/components/modal/slider.jsx
@@ -0,0 +1,10 @@
+// Это будет слайдер для фоток в модалке
+
+/*
+import React from "react";
+
+const Slider = () => {
+
+}
+export default Slider;
+*/
