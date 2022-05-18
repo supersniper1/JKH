dd393753186d38b9ab8310207fe5a245af813c62
Denis Ustinov <92351987+supersniper1@users.noreply.github.com>
2022-05-18 22:43:28 +0500

    action was renamed

--- src/api/post.api.js
+++ src/api/post.api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiURL = 'http://10.3.21.206:8080'
 const loginRequest = (dataValue, handleLogin, setIncorrectPassword) => {
   axios.post(`${apiURL}/api/v1/auth/login`, dataValue)
-    .then ((response) => action(response.data.token))
+    .then ((response) => handleLogin(response.data.token))
     .catch(setIncorrectPassword(true))
 }
 
